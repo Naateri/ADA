00b25b6927cdd44311c3dbb955cbb1ef36afe0e7
Renato Postigo <renato.postigo@ucsp.edu.pe>
2018-09-19 20:56:43 -0500

    gg ada

--- practica-1909.cpp
+++ practica-1909.cpp
@@ -9,6 +9,8 @@ using namespace std;
 
 typedef vector<int> int_vec;
 
+//helpful functions
+
 void fill_vec(int_vec& A, int size, int max = MAXI){
 	srand( time(NULL) );
 	for (int i = 0; i < size; i++){
@@ -29,13 +31,14 @@ void swap(int& a, int& b){
 	b = temp;
 }
 
+//Sorts
+
 void insert_sort(int_vec& A){
 	int temp, j;
 	for (int i = 1; i < A.size(); i++){
 		temp = A.at(i);
 		j = i-1;
 		while ( j >= 0 && A.at(j) > temp ){
-			//swap( A.at(j+1), A.at(j) );
 			A.at(j+1) = A.at(j);
 			j--;
 		}
@@ -43,11 +46,7 @@ void insert_sort(int_vec& A){
 	}
 }
 
-/*int_vec merge(int_vec A, int start, int mid, int end){
-	int_vec res;
-	int a_temp = A.at(start), b_temp = A.at(mid);
-		
-}*/
+//Regular mergesort
 
 int_vec merge(int_vec A, int_vec B){
 	int_vec result;
@@ -65,28 +64,19 @@ int_vec merge(int_vec A, int_vec B){
 	return result;
 }
 
-/*int_vec mergesort(int_vec A, int start, int end){
-	int_vec res;
-	if (start < end){
-		int mid = (start+end)>>1;
-		mergesort(A, start, mid);
-		mergesort(A, mid+1, end);
-		merge(A, start, mid, end);
-	}
-	
-}*/
-
 int_vec mergesort(int_vec A){
 	int_vec left, right, result;
 	if (A.size() <= 1){
 		return A;
 	}
 	int mid = (A.size())>>1;
-	copy(begin(A), begin(A) + mid, back_inserter(left) );
-	copy(begin(A) + mid, end(A), back_inserter(right) );
+	copy(A.begin(), A.begin() + mid, back_inserter(left) );
+	copy(A.begin() + mid, A.end(), back_inserter(right) );
 	result = merge(mergesort(left), mergesort(right));
 	return result;
 }
+
+//mergesort where <=k elements get sorted with insertion sort
 
 int_vec mergesort_k(int_vec A, int k){
 	int_vec left, right, result;
@@ -101,27 +91,64 @@ int_vec mergesort_k(int_vec A, int k){
 	return result;
 }
 
+//mergesort using half the memory when it can
 
-/*void mergesort_k(int_vec& A, int start, int end, int k){
-	if (start < end || (end-start) >= k){
-		int mid = (start+end)>>1;
-		mergesort(A, start, mid);
-		mergesort(A, mid+1, end);
-		merge(A, start, mid, end);
+void merge_n2(int_vec& A, int start, int mid, int end){
+	int tempS, tempE;
+	tempS = start;
+	tempE = mid+1;
+	while (tempS < mid+1 && tempE < end){
+		if ( A.at(tempS) < A.at(tempE) ){ //usual merge
+			tempS++;
+		} else {
+			swap(A.at(tempS), A.at(tempE));
+			tempE++;
+			tempS++;
+		}
 	}
-}*/
+	while (tempS < end){
+		if (A.at(tempS) < A.at(tempS + 1)) return;
+		swap(A.at(tempS), A.at(tempS+1));
+		tempS++;
+	}
+}
+
+void mergesort_n2(int_vec& A, int start, int end){
+	if (start < end){
+		int mid = (start+end)>>1;
+		mergesort_n2(A, start, mid);
+		mergesort_n2(A, mid+1, end);
+		merge_n2(A, start, mid, end);
+	}
+}
 
 int main(int argc, char *argv[]) {
 	
-	int_vec A;
-	fill_vec(A, 128, 500);
+	int_vec A, A_merge, A_mergeK, A_mergeN2;
+	/*char ask;
+	int k;
+	cout << "Todas las pruebas se realizarán con 1k (1000) datos.\n";
+	fill_vec(A, 1000, 10000);
+	cout << "Desea ver los datos generados? (y/n)"; cin >> ask; 
+	if (ask == 'y'){
+		print_vec(A);
+	}
+	cout << "Presione cualquier tecla para continuar\n"; cin >> ask;
+	cout << "Mergesort normal\n";
+	A_merge = mergesort(A);
+	print_vec(A_merge);
+	cout << "Presione cualquier tecla para continuar\n"; cin >> ask;
+	cout << "Mergesort k (insert sort)\n";
+	cout << "Ingrese un k\n"; cin >> k;
+	A_mergeK = mergesort_k(A, k);
+	print_vec(A_mergeK);
+	cout << "Presione cualquier tecla para continuar\n"; cin >> ask;
+	A.erase(A.begin(), A.end());
+	*/
+	fill_vec(A, 10, 1000);
 	print_vec(A);
-	//insert_sort(A);
-	//A = mergesort(A);
-	//cout << A.size() << endl;
-	A = mergesort_k(A, 8);
+	mergesort_n2( A, 0, A.size() - 1 );
 	print_vec(A);
-	
 	return 0;
 }
 
